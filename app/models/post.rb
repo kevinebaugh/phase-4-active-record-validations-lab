8be85bb2914817c56aff9916d826075fc15e32ce
Kevin Ebaugh <kevinebaugh@gmail.com>
2022-04-07 22:01:20 -0400

    commit tree be0daeb69b0ec6da2c48f6a11d3a2187b89f5124

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,24 @@
 class Post < ApplicationRecord
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: ['Fiction', 'Non-Fiction'] }
+  validate :ensure_click_bait
+
+  private
+
+  def ensure_click_bait
+    patterns = [
+      /Won't Believe/i,
+      /Secret/i,
+      /Top \d/i,
+      /Guess/i
+    ]
+    
+    if patterns.any?{ |pattern| pattern.match(title) }
+      true
+    else
+      errors.add(:title, "needs to be clickbait")
+    end
+  end
 end
